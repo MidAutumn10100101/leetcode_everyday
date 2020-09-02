303e2375d1286f8c09f579743ce6b411c21c57ae
HZY <1606240131@qq.com>
2020-09-02 20:38:04 +0800

    剑指53-2--0~n-1缺失的数字

--- Java/src/8-19-剑指53-2--0~n-1缺失的数字/Solution.java
+++ Java/src/8-19-剑指53-2--0~n-1缺失的数字/Solution.java
@@ -1,5 +1,20 @@
 public class Solution {
     public static void main(String[] args) {
         System.out.println("solution");
+        int[] nums = {0};
+        int res = missingNumber(nums);
+        System.out.println(res);
+    }
+
+    public static int missingNumber(int[] nums) {
+        int i = 0;
+        for (int num:
+             nums) {
+            if(num == i)
+                i++;
+            else
+                return i;
+        }
+        return i;
     }
 }
