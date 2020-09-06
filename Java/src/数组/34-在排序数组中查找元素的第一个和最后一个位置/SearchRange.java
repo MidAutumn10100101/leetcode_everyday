096cbbf6758490bb1778208ce5807b21f99691d9
HZY <1606240131@qq.com>
2020-09-06 22:29:08 +0800

    9-6

---
+++ Java/src/数组/34-在排序数组中查找元素的第一个和最后一个位置/SearchRange.java
@@ -0,0 +1,11 @@
+public class SearchRange {
+    public static void main(String[] args) {
+        System.out.println("solution");
+
+    }
+    public static int[] searchRange(int[] nums,int target){
+        int len = nums.length;
+        if(len == 1)
+    }
+
+}
